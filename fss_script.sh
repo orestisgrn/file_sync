a901a724a7a344a5ba7e42fbd6257d5693876e20
Orestis Gkaranis <sdi2200282@di.uoa.gr>
2025-05-02 04:29:22 +0300

    fss_script except purge

--- fss_script.sh
+++ fss_script.sh
@@ -26,11 +26,9 @@ for arg in "$@"; do
         command_given=T
         next_is_command=T
     elif [ "$next_is_path" = "T" ]; then
-        echo "Path: $arg"
         path_name=$arg
         next_is_path=F
     elif [ "$next_is_command" = "T" ]; then
-        echo "Command: $arg"
         command_name=$arg
         next_is_command=F
     fi   
@@ -46,15 +44,89 @@ if [ "$command_given" = "F" ] || [ "$next_is_command" = "T" ]; then
     exit 1
 fi
 
-if [ -f "$path_name" ]; then
-    while IFS= read -r line; do # match five brackets
-        echo "$line" | grep -P "^\[[^\]]+\] \[[^\]]+\] \[[^\]]+\] \[[^\]]+\] \[[^\]]+\]$" > /dev/null
-        if [ $? -eq 0 ]; then
-            echo "$line" | awk -F'[\\[\\]]' '{for(i=2;i<=NF;i+=2) print $i}'
-            IFS= read -r success_code
-            echo "$success_code" | awk -F'[\\[\\]]' '{print $2}'
+declare -A sync_info_arr
+
+if [ "$command_name" = "listAll" ]; then
+    if [ -f "$path_name" ]; then
+        while IFS= read -r line; do # match five brackets
+            echo "$line" | grep -P "^\[[^\]]+\] \[[^\]]+\] \[[^\]]+\] \[[^\]]+\] \[[^\]]+\]$" > /dev/null
+            if [ $? -eq 0 ]; then
+                source=$(echo "$line" | awk -F'[\\[\\]]' '{print $4}')
+                IFS= read -r success_code
+                sync_info_arr["$source"]="${line} ${success_code}"
+            fi
+        done < "$path_name"
+    else
+        echo "Invalid file: $path_name"
+        exit 1
+    fi
+    for record in "${sync_info_arr[@]}"; do
+        echo "$record" | awk -F'[\\[\\]]' '{print $4, "->", $6, "[Last Sync:", $2"]", "["$12"]"}'
+    done
+elif [ "$command_name" = "listMonitored" ]; then
+    declare -A stopped
+    if [ -f "$path_name" ]; then
+        while IFS= read -r line; do # match five brackets
+            echo "$line" | grep -P "^\[[^\]]+\] \[[^\]]+\] \[[^\]]+\] \[[^\]]+\] \[[^\]]+\]$" > /dev/null
+            if [ $? -eq 0 ]; then
+                source=$(echo "$line" | awk -F'[\\[\\]]' '{print $4}')
+                IFS= read -r success_code
+                sync_info_arr["$source"]="${line} ${success_code}"
+            else 
+                read -r -a words <<< "$line"
+                echo "$line" | grep "Syncing directory" > /dev/null
+                if [ $? -eq 0 ]; then
+                    unset stopped["${words[4]}"]        # See format of syncing dir
+                else
+                    echo "$line" | grep "Monitoring stopped" > /dev/null
+                    if [ $? -eq 0 ]; then
+                        stopped["${words[5]}"]=1
+                    fi
+                fi
+            fi
+        done < "$path_name"
+    else
+        echo "Invalid file: $path_name"
+        exit 1
+    fi
+    for key in "${!sync_info_arr[@]}"; do
+        if [[ ! -v stopped["$key"] ]]; then
+            echo "${sync_info_arr["$key"]}" | awk -F'[\\[\\]]' '{print $4, "->", $6, "[Last Sync:", $2"]"}'
         fi
-    done < "$path_name"
+    done
+elif [ "$command_name" = "listStopped" ]; then      # same as listMonitored, but prints only stopped
+    declare -A stopped
+    if [ -f "$path_name" ]; then
+        while IFS= read -r line; do # match five brackets
+            echo "$line" | grep -P "^\[[^\]]+\] \[[^\]]+\] \[[^\]]+\] \[[^\]]+\] \[[^\]]+\]$" > /dev/null
+            if [ $? -eq 0 ]; then
+                source=$(echo "$line" | awk -F'[\\[\\]]' '{print $4}')
+                IFS= read -r success_code
+                sync_info_arr["$source"]="${line} ${success_code}"
+            else 
+                read -r -a words <<< "$line"
+                echo "$line" | grep "Syncing directory" > /dev/null
+                if [ $? -eq 0 ]; then
+                    unset stopped["${words[4]}"]        # See format of syncing dir
+                else
+                    echo "$line" | grep "Monitoring stopped" > /dev/null
+                    if [ $? -eq 0 ]; then
+                        stopped["${words[5]}"]=1
+                    fi
+                fi
+            fi
+        done < "$path_name"
+    else
+        echo "Invalid file: $path_name"
+        exit 1
+    fi
+    for key in "${!sync_info_arr[@]}"; do
+        if [[ -v stopped["$key"] ]]; then
+            echo "${sync_info_arr["$key"]}" | awk -F'[\\[\\]]' '{print $4, "->", $6, "[Last Sync:", $2"]"}'
+        fi
+    done
+elif [ "$command_name" = "purge" ]; then
+    echo "purge"
 else
     echo "Invalid file: $path_name"
 fi
